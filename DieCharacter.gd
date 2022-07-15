073b933f57c88c60000a0a56e4d63b20373c6e0f
jkamins7 <jkaminsky@jhu.edu>
2022-07-15 16:43:13 -0400

    Forgot to commit this file

--- DieCharacter.gd
+++ DieCharacter.gd
@@ -10,6 +10,7 @@ var current_state = [Vector3(0,0,1), Vector3(1,0,0)]
 
 var state = "WAIT_FOR_INPUT"
 var event_array = []
+var my_name: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -36,7 +37,9 @@ func move_right():
 	current_state[0] = current_state[0].rotated(Vector3(0,1,0), PI/2)
 	current_state[1] = current_state[1].rotated(Vector3(0,1,0), PI/2)
 	
-	event_array.push_back(PushEvent.new())
+	my_name = "Player"
+	event_array.push_back(PushEvent.new(self))
+	event_array.push_back(PushEvent.new(self))
 
 	
 	move_and_slide_with_snap(Vector2(1,0)*MOVEMENT_SIZE * 100,Vector2(MOVEMENT_SIZE,MOVEMENT_SIZE))
