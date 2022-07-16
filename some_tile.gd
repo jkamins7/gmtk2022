7b1dd67788b4d6174eb405babd6ec30f5bb5fd05
Kenny Denmark <denmark1@umbc.edu>
2022-07-15 21:53:25 -0400

    added dice icon to tiles

--- some_tile.gd
+++ some_tile.gd
@@ -9,6 +9,7 @@ extends Sprite
 export(int) var permitted_number = 3
 export(Vector2) var parent_transform = Vector2.UP
 onready var player = $AnimationPlayer
+onready var number_display = $Number
 var active = false
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +17,12 @@ func _ready():
 	var player_node = get_owner().find_node("Player")
 	player_node.connect("change_face", self, "_on_change_face")
 	find_node("Hitbox").parent_transform = 	Vector2.UP.rotated(self.global_rotation_degrees*2*PI/360)
+	find_node("Hitbox").permitted_number = permitted_number
+	
+	number_display.set_frame(permitted_number-1)
+	 # hacky fix since the parent's rotation applies to this
+	number_display.rotate(-self.global_rotation_degrees*2*PI/360)
+
 
 
 func _on_change_face(old_face, new_face):
@@ -29,4 +36,4 @@ func _physics_process(delta):
 		player.play("active")
 	else:
 		player.play("idle")
-	
+
