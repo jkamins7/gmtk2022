ed6d96876d33c5f0a7894f0a1543cc708085ae06
jkamins7 <jkaminsky@jhu.edu>
2022-07-16 14:09:59 -0400

    Another level

--- EndTile.gd
+++ EndTile.gd
@@ -1,5 +1,10 @@
 extends Sprite
 
+export(int) var permitted_number = 0
+
+onready var hitbox = $Hitbox
+
 func _ready():
 	var player_node = get_owner().find_node("Player")
 	player_node.connect("change_face", self, "_on_change_face")
+	hitbox.permitted_number = permitted_number
