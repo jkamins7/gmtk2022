0983e1c8d63f95e0c199ff2d838fcab9b050d08f
Kenny Denmark <denmark1@umbc.edu>
2022-07-16 16:01:42 -0400

    added alternate compass rose graphic

--- CompassRose.gd
+++ CompassRose.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var player_node
 
+onready var background = $background
 onready var rs = $RIGHT_sprite
 onready var ls = $LEFT_sprite
 onready var us = $UP_sprite
@@ -17,17 +18,26 @@ func _ready():
 
 func update_compass_in_direction(direction, sprite):
 	var upward_side_in_direction = player_node.return_upward_side_in_direction(reverse_direction * direction)
+
+	#if upward_side_in_direction[0]:
+	sprite.set_frame(upward_side_in_direction[1] - 1)
 	if upward_side_in_direction[0]:
-		sprite.set_frame(upward_side_in_direction[1] - 1)
-		return
+		sprite.modulate.a = 1
+	else:
+		sprite.modulate.a=.5
+		#return
 	# JK : This probably should be 7, a special NAN pip set, and greyed out
-	sprite.set_frame(6)
+	#sprite.set_frame(6)
 	
 func _on_select_pressed():
 	self.reverse_direction *= -1
 	_on_change_face(null,null)
 	
 func _on_change_face(old_face, new_face):
+	if reverse_direction == 1:
+		background.set_frame(0)
+	else:
+		background.set_frame(1)
 	update_compass_in_direction(Vector2.RIGHT,rs)
 	update_compass_in_direction(Vector2.LEFT,ls)
 	update_compass_in_direction(Vector2.UP,us)
