d86042bdcdf71d1844120f7738c56a972584cda4
jkamins7 <jkaminsky@jhu.edu>
2022-07-16 14:59:07 -0400

    Fixed mandatory event multicounting bug

--- MandatoryEvent.gd
+++ MandatoryEvent.gd
@@ -4,13 +4,15 @@ class_name MandatoryEvent
 var thing_with_counter
 var thing_to_change_color
 var thing_that_moved
+var thing_to_toggle_activated
 var valid_number
 var color_to_change_to
 
-func _init(thing_that_moved, thing_with_counter, thing_to_change_color, valid_number = 0, target_color = Color(0,0,1)):
+func _init(thing_that_moved, thing_with_counter, thing_to_change_color, thing_to_toggle_activated, valid_number = 0, target_color = Color(0,0,1)):
 	self.thing_that_moved = thing_that_moved
 	self.thing_with_counter = thing_with_counter
 	self.thing_to_change_color = thing_to_change_color
+	self.thing_to_toggle_activated = thing_to_toggle_activated
 	self.valid_number = valid_number
 	self.color_to_change_to = target_color
 	self.name = 'mandatory'
@@ -24,10 +26,11 @@ func matches(num):
 
 func permitted():
 	var current_side = self.thing_that_moved.return_upward_side()
-	return matches(current_side)
+	return matches(current_side) && (!thing_to_toggle_activated.has_activated)
 
 func run():
 	if permitted():
 		self.thing_to_change_color.set_modulate(color_to_change_to)
 		thing_with_counter.mandatory_events_remaining -= 1
 		print(thing_with_counter.mandatory_events_remaining)
+		thing_to_toggle_activated.has_activated = true
--- MandatoryTilePush.gd
+++ MandatoryTilePush.gd
@@ -3,6 +3,7 @@ extends Area2D
 export(int) var permitted_number = 0
 export(Color) var color_to_change_to = Color(.3,.9,.4)
 var parent_transform
+var has_activated = false
 
 func _ready():
 	connect("area_entered", self, "_on_Hitbox_area_entered")
@@ -17,6 +18,7 @@ func _on_Hitbox_area_entered(area):
 		area.get_parent(),
 		area.get_parent(),
 		self.get_parent(),
+		self,
 		permitted_number,
 		color_to_change_to
 	))
